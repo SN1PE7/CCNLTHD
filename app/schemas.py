84e937a8fd3858fb1eda20e172de25153277925a
Cao Nguyen Duc Huy <duchuy231132@gmail.com>
2026-02-26 21:39:01 +0700

    Commit

---
+++ app/schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import List
+
+class HousingData(BaseModel):
+    longitude: float
+    latitude: float
+    housing_median_age: float
+    total_rooms: float
+    total_bedrooms: float
+    population: float
+    households: float
+    median_income: float
+    ocean_proximity: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "longitude": -122.23,
+                "latitude": 37.88,
+                "housing_median_age": 41.0,
+                "total_rooms": 880.0,
+                "total_bedrooms": 129.0,
+                "population": 322.0,
+                "households": 126.0,
+                "median_income": 8.3252,
+                "ocean_proximity": "NEAR BAY"
+            }
+        }
+
+class PredictionResult(BaseModel):
+    prediction: float
+    inference_time: str
