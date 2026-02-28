3f79dfd6f5ecd3f6ce6f82d3bd5266fa803c8a6e
Cao Nguyen Duc Huy <duchuy231132@gmail.com>
2026-02-28 16:35:54 +0700

    test

--- app/main.py
+++ app/main.py
@@ -1,27 +1,41 @@
-import joblib
 import pandas as pd
+import joblib
+import time
 from fastapi import FastAPI
+from typing import List
 from app.schemas import HousingData, PredictionResult
 
-app = FastAPI()
+app = FastAPI(title="California House Price Prediction API")
 
+# Bien toan cuc de giu model
 model = None
 
+# Load model
 @app.on_event("startup")
 async def load_model():
     global model
-    try:
-        model_path = 'model/model.joblib'
-        
-        model = joblib.load(model_path)
-        
-        print(f"--- SUCCESS: Model loaded from {model_path} ---")
-    except Exception as e:
-        print(f"--- ERROR: Could not load model. Details: {e} ---")
+    model = joblib.load("model/model.joblib")
+    print("load thanh cong")
 
 @app.get("/")
-async def root():
-    return {
-        "message": "API đang hoạt động!",
-        "model_status": "Ready" if model is not None else "Not Loaded"
-    }
+def read_root():
+    return {"message": "qua /docs"}
+
+# endpoint
+@app.post("/predict", response_model=List[PredictionResult])
+async def predict(data: List[HousingData]):
+    start_time = time.time()
+    
+    input_df = pd.DataFrame([item.dict() for item in data])
+
+    predictions = model.predict(input_df)
+
+    end_time = time.time()
+    duration = f"{(end_time - start_time) * 1000:.2f} ms"
+
+    results = [
+        PredictionResult(prediction=pred, inference_time=duration) 
+        for pred in predictions
+    ]
+    
+    return results
